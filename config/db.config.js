bc74a6b78546c766ea33c3c53b62f985d7f47b71
YashrajPawar <yashrajpawar33@gmail.com>
2023-04-20 13:06:02 +0530

    updated package json file and configuration file

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    url: 'mongodb+srv://YashrajPawar:Renukaapril01@cluster0.mjamzjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    url: 'mongodb+srv://YashrajPawar:<password>@cluster0.mjamzjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 }
